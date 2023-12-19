5074eac4a2419665b364f973ca395b4df740c646
Abhishek Goyal <58354473+abhishekgoyal-a11y@users.noreply.github.com>
2023-12-19 19:04:52 +0530

    login test data and test cases completed

--- src/test/java/utilities/DataProviders.java
+++ src/test/java/utilities/DataProviders.java
@@ -6,34 +6,24 @@ import org.testng.annotations.DataProvider;
 
 public class DataProviders {
 
-	//DataProvider 1
-	
+
 	@DataProvider(name="LoginData")
 	public String [][] getData() throws IOException
-	{
-		String path=".\\testData\\Opencart_LoginData.xlsx";//taking xl file from testData
-		
-		ExcelUtility xlutil=new ExcelUtility(path);//creating an object for XLUtility
-		
-		int totalrows=xlutil.getRowCount("Sheet1");	
-		int totalcols=xlutil.getCellCount("Sheet1",1);
-				
-		String logindata[][]=new String[totalrows][totalcols];//created for two dimension array which can store the data user and password
-		
-		for(int i=1;i<=totalrows;i++)  //1   //read the data from xl storing in two deminsional array
+	{	
+		String LoginSheetName = "Login";
+		String path=".\\testData\\Opencart_TestData.xlsx";
+		ExcelUtility xlutil=new ExcelUtility(path);		
+		int totalrows=xlutil.getRowCount(LoginSheetName);	
+		int totalcols=xlutil.getCellCount(LoginSheetName,1);
+		String logindata[][]=new String[totalrows][totalcols];	
+		for(int i=1;i<=totalrows;i++)
 		{		
-			for(int j=0;j<totalcols;j++)  //0    i is rows j is col
+			for(int j=0;j<totalcols;j++)
 			{
-				logindata[i-1][j]= xlutil.getCellData("Sheet1",i, j);  //1,0
+				logindata[i-1][j]= xlutil.getCellData(LoginSheetName,i, j);
 			}
 		}
-	return logindata;//returning two dimension array
+	return logindata;
 				
 	}
-	
-	//DataProvider 2
-	
-	//DataProvider 3
-	
-	//DataProvider 4
 }
